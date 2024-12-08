faba41178f45f68e94a78944b7cd75f8464b0955
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2024-12-08 14:50:10 +0100

    Day 2, puzzle 2

--- 2024/go/day_02/main.go
+++ 2024/go/day_02/main.go
@@ -69,9 +69,123 @@ func calcSafeReports(reports [][]int) int {
 	return safeReportsNum
 }
 
+func dumpLevel(report []int, level int) []int {
+	var reducedReport []int
+	if level == len(report)-1 {
+		reducedReport = append(reducedReport, report[:level]...)
+		return reducedReport
+	}
+
+	reducedReport = append(reducedReport, report[:level]...)
+	reducedReport = append(reducedReport, report[level+1:]...)
+	return reducedReport
+}
+
+func isSafeReportWithProblemDumpener(report []int) bool {
+	return (levelDiffInRangeWithProblemDumpener(report) && isOrderedWithProblemDumpener(report))
+}
+
+func levelDiffInRangeWithProblemDumpener(report []int) bool {
+	for i := len(report) - 1; i > 0; i-- {
+		if !diffInRange(report[i], report[i-1]) {
+			modifiedReport := dumpLevel(report, i)
+			for j := len(modifiedReport) - 1; j > 0; j-- {
+				if diffInRange(modifiedReport[j], modifiedReport[j-1]) {
+					continue
+				} else {
+					newModifiedReport := dumpLevel(report, i-1)
+					for k := len(newModifiedReport) - 1; k > 0; k-- {
+						if !diffInRange(newModifiedReport[k], newModifiedReport[k-1]) {
+							return false
+						}
+					}
+
+					return true
+				}
+			}
+
+			return true
+		}
+	}
+
+	return true
+}
+
+func levelDiffInRange(report []int) bool {
+	for i := len(report) - 1; i > 0; i-- {
+		if !diffInRange(report[i], report[i-1]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isOrderedWithProblemDumpener(report []int) bool {
+	isAscending := report[0] < report[1]
+	if isAscending {
+		for i := len(report) - 1; i > 0; i-- {
+			if report[i] > report[i-1] {
+				continue
+			}
+
+			modifiedReport := dumpLevel(report, i)
+			if slices.IsSorted(modifiedReport) && levelDiffInRange(modifiedReport) {
+				continue
+			}
+
+			newModifiedReport := dumpLevel(report, i-1)
+			if !slices.IsSorted(newModifiedReport) || !levelDiffInRange(newModifiedReport) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	for i := len(report) - 1; i > 0; i-- {
+		if report[i] < report[i-1] {
+			continue
+		}
+
+		modifiedReport := dumpLevel(report, i)
+		slices.Reverse(modifiedReport)
+		if slices.IsSorted(modifiedReport) {
+			continue
+		}
+
+		newModifiedReport := dumpLevel(report, i-1)
+		slices.Reverse(newModifiedReport)
+		if !slices.IsSorted(newModifiedReport) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func diffInRange(l, r int) bool {
+	diff := math.Abs(float64(l - r))
+	return diff <= 3 && diff >= 1
+}
+
+func calcSafeReportsWithProblemDumpener(reports [][]int) int {
+	var safeReportsNum int
+	for _, report := range reports {
+		if isSafeReportWithProblemDumpener(report) {
+			safeReportsNum++
+		}
+	}
+
+	return safeReportsNum
+}
+
 func main() {
 	in := readInputFile(input)
 	reports := getReports(in)
 	safeReports := calcSafeReports(reports)
 	fmt.Printf("Safe report number: %d\n", safeReports)
+
+	safeReportsWithProblemDumpener := calcSafeReportsWithProblemDumpener(reports)
+	fmt.Printf("Safe reports with Problem Dumpener: %d\n", safeReportsWithProblemDumpener)
 }
