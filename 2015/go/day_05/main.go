433eb6eb9792d9427b1b8ff7f84bfbc141663725
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2024-12-16 15:54:49 +0100

    2015, day 5, puzzle 2

--- 2015/go/day_05/main.go
+++ 2015/go/day_05/main.go
@@ -65,6 +65,56 @@ func countNiceStrings(s string) int {
 	return niceStringCount
 }
 
+func containsPairTwice(s string) bool {
+	if len(s) < 3 {
+		return false
+	}
+
+	letters := strings.Split(s, "")
+
+	for i := 0; i < len(letters)-3; i++ {
+		next := i + 1
+		pair := letters[i] + letters[next]
+		if strings.Contains(s[i+2:], pair) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasLetterBetweenSameLetters(s string) bool {
+	if len(s) < 3 {
+		return false
+	}
+
+	letters := strings.Split(s, "")
+	for i := 1; i < len(letters)-1; i++ {
+		if letters[i-1] == letters[i+1] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isNiceNew(s string) bool {
+	return containsPairTwice(s) && hasLetterBetweenSameLetters(s)
+}
+
+func countNiceStringsNew(s string) int {
+	var niceStringCount int
+	strs := strings.Split(s, stringsSep)
+
+	for _, str := range strs {
+		if isNiceNew(str) {
+			niceStringCount += 1
+		}
+	}
+
+	return niceStringCount
+}
+
 func main() {
 	data, err := os.ReadFile(inputFile)
 	if err != nil {
@@ -73,4 +123,6 @@ func main() {
 
 	input := string(data)
 	fmt.Printf("There are %d nice strings in Santa's text file\n", countNiceStrings(input))
+
+	fmt.Printf("There are %d nice strings in Santa's text file with a new model\n", countNiceStringsNew(input))
 }
