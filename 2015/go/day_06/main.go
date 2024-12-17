1f759f6ef9dfcd56a36883332351b3d2c1fd6d6a
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2024-12-17 16:53:55 +0100

    2015, day 6, working with interfaces

--- 2015/go/day_06/main.go
+++ 2015/go/day_06/main.go
@@ -13,6 +13,13 @@ const (
 	lineSep   = "\n"
 )
 
+type light interface {
+	turnOff()
+	turnOn()
+	toggle()
+	indication() int
+}
+
 type Light struct {
 	isOn bool
 }
@@ -29,13 +36,29 @@ func (l *Light) turnOff() {
 	l.isOn = false
 }
 
-type Grid struct {
-	lights [][]Light
+func (l Light) indication() int {
+	if l.isOn {
+		return 1
+	}
+	return 0
 }
 
-func CreateGrid(length, width int) *Grid {
-	grid := make([][]Light, length)
-	rows := make([]Light, length*width)
+type Grid struct {
+	lights [][]light
+}
+
+func CreateGrid(length, width int, lightType string) *Grid {
+	grid := make([][]light, length)
+
+	switch lightType {
+	case "Light":
+		rows := make([]Light, 1000)
+	case "LightNew":
+		rows := make([]LightNew, 1000)
+	default:
+		log.Fatalf("Unknown light type: %s\n", lightType)
+	}
+
 	for i := range grid {
 		grid[i], rows = rows[:width], rows[width:]
 	}
@@ -45,17 +68,15 @@ func CreateGrid(length, width int) *Grid {
 	}
 }
 
-func (g Grid) countLightsOn() int {
-	var lightsOn int
+func (g Grid) countLightsOnIndications() int {
+	var lightsIndications int
 	for _, row := range g.lights {
 		for _, light := range row {
-			if light.isOn {
-				lightsOn += 1
-			}
+			lightsIndications += light.indication()
 		}
 	}
 
-	return lightsOn
+	return lightsIndications
 }
 
 func (g *Grid) executeInstruction(instruction *Instruction) {
@@ -147,48 +168,8 @@ func (l *LightNew) toggle() {
 	l.brightness += 2
 }
 
-type GridNew struct {
-	lights [][]LightNew
-}
-
-func CreateGridNew(length, width int) *GridNew {
-	grid := make([][]LightNew, length)
-	rows := make([]LightNew, length*width)
-	for i := range grid {
-		grid[i], rows = rows[:width], rows[width:]
-	}
-
-	return &GridNew{
-		lights: grid,
-	}
-}
-
-func (g *GridNew) executeInstruction(instruction *Instruction) {
-	for i := instruction.startLength; i <= instruction.endLength; i++ { //rows
-		for j := instruction.startWidth; j <= instruction.endWidth; j++ { // lights in a row
-			switch instruction.operation {
-			case "turn on":
-				g.lights[i][j].turnOn()
-			case "turn off":
-				g.lights[i][j].turnOff()
-			case "toggle":
-				g.lights[i][j].toggle()
-			default:
-				log.Fatalf("Unknown operation %q\n", instruction.operation)
-			}
-		}
-	}
-}
-
-func (g GridNew) countBrightness() int {
-	var brightness int
-	for _, row := range g.lights {
-		for _, light := range row {
-			brightness += light.brightness
-		}
-	}
-
-	return brightness
+func (l LightNew) indication() int {
+	return l.brightness
 }
 
 func main() {
@@ -198,15 +179,15 @@ func main() {
 	}
 
 	lines := strings.Split(string(data), lineSep)
-	g := CreateGrid(1000, 1000)
-	g_02 := CreateGridNew(1000, 1000)
+	firstGrid := CreateGrid(1000, 1000, "Light")
+	secondGrid := CreateGrid(1000, 1000, "LightNew")
 
 	for _, line := range lines {
 		ins := parseInstruction(line)
-		g.executeInstruction(ins)
-		g_02.executeInstruction(ins)
+		firstGrid.executeInstruction(ins)
+		secondGrid.executeInstruction(ins)
 	}
 
-	fmt.Printf("There are %d lights lit on\n", g.countLightsOn())
-	fmt.Printf("The total brightness of all lights combined is %d\n", g_02.countBrightness())
+	fmt.Printf("There are %d lights lit on\n", firstGrid.countLightsOnIndications())
+	fmt.Printf("The total brightness of all lights combined is %d\n", secondGrid.countLightsOnIndications())
 }
